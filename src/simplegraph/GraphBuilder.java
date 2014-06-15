5157583c1b8fa0db7aefb221366361633a3cea55
Petr Kramar <kramar@masquerade.cz>
2014-06-15 18:12:40 +0200

    Initial version

---
+++ src/simplegraph/GraphBuilder.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simplegraph;
+
+/**
+ * Builder for all possible graphs
+ * @author petr
+ */
+public class GraphBuilder {
+
+    
+    
+}
