0ad2557ffab2351efc5845fcf32802adefedc981
Martin Kramar <martin.kramar@gmail.com>
2014-06-28 22:02:19 +0200

    Added painting grid labels.

--- src/simpledraw/GraphPane.java
+++ src/simpledraw/GraphPane.java
@@ -12,9 +12,11 @@ import java.awt.Rectangle;
 import simpledraw.drawable.DrawArea;
 
 /**
- * This class represent basic pane for graph.
- * It enables set colors, grids,...
- * @author kramarm
+ * This class is used as base panel for drawing graphs.
+ * It uses {@link DrawArea} for painting at screen and
+ * allows to display borders, grids, labels,...
+ * and catches scroll bar movement in the method scrollBarMoved
+ * @author Martin Kramar
  */
 public class GraphPane extends DrawArea{
     
@@ -22,19 +24,43 @@ public class GraphPane extends DrawArea{
     private final int GRID_SIZE_DEFAULT = 70;
     private final int GRID_OFFSET_DEFAULT = 20;
     
+    /**
+     * It is a bordered area inside scrollPane.
+     * All {@link Drawable} objects which are visible at scrollpane but are
+     * outside this area are not painted because this outside are is used for
+     * painting of grid labels
+     */
     private Rectangle borderRectangle;
     private boolean verticalGridFlag = true;
     private boolean horizontalGridFlag = true;
+    private boolean verticalLabelsFlag = true;
+    private boolean horizontalLabelsFlag = true;
     
     //spaces betwenn boundary of displayed area and component
     private int spaceNorth = BORDER_SPACE;
     private int spaceSouth = BORDER_SPACE;
     private int spaceEast = BORDER_SPACE;
     private int spaceWest = BORDER_SPACE;
+    
+    //*pixels between grid lines*/
+    private int horizontalGridGap = GRID_SIZE_DEFAULT;
+    //*pixels between border and first grid line*/
+    private int horizontalGridOffset = GRID_OFFSET_DEFAULT;
+    //*pixels between grid lines*/
+    private int verticalGridGap = GRID_SIZE_DEFAULT;
+    //*pixels between border and first grid line*/
+    private int verticalGridOffset = GRID_OFFSET_DEFAULT;
+    
+    private String[] horizontalLabels = null;
+    private String[] verticalLabels = null;
    
     
     
-    
+    /**
+     * Creates graphPane with specified drawing area size
+     * @param width in pixels
+     * @param height in pixels
+     */
     public GraphPane(int width, int height){
         super(width, height);   
     }
@@ -62,10 +88,12 @@ public class GraphPane extends DrawArea{
         paintAreaBorder(g);
         
         if(verticalGridFlag)
-            paintVerticalGrid(g, GRID_SIZE_DEFAULT, GRID_OFFSET_DEFAULT);
+            paintVerticalGrid(g, verticalGridGap, verticalGridOffset);
         
         if(horizontalGridFlag)
-            paintHorizontalGrid(g, GRID_SIZE_DEFAULT, GRID_OFFSET_DEFAULT);
+            paintHorizontalGrid(g, horizontalGridGap, horizontalGridOffset);
+        
+        paintLabels(g);
         
     }
     
@@ -107,11 +135,12 @@ public class GraphPane extends DrawArea{
         g.setColor(Color.GRAY);
         
         int lastX = borderRectangle.x + borderRectangle.width;
-        int x = borderRectangle.x + offset;
-        for(int i=0; x<lastX ;i++){
-            x = borderRectangle.x + offset + i*gap;
+        int x;
+        for(int i=0; (x = borderRectangle.x + verticalGridOffset + i*verticalGridGap)<lastX ;i++){
             if(x>borderRectangle.x)
                 g.drawLine(x, borderRectangle.y, x, borderRectangle.y+borderRectangle.height);
+            
+            x = borderRectangle.x + offset + i*gap;
         }
         
         g.setColor(cOrig);
@@ -130,9 +159,8 @@ public class GraphPane extends DrawArea{
         g.setColor(Color.GRAY);
         
         int lastY = borderRectangle.y + borderRectangle.height;
-        int y = borderRectangle.y + offset;
-        for(int i=0; y<lastY ;i++){
-            y = borderRectangle.y + offset + i*gap;
+        int y;
+        for(int i=0; (y = borderRectangle.y + horizontalGridOffset + i*horizontalGridGap)<lastY ;i++){      
             if(y>borderRectangle.y)
                 g.drawLine(borderRectangle.x, y, borderRectangle.x+borderRectangle.width, y);
         }
@@ -141,8 +169,41 @@ public class GraphPane extends DrawArea{
     }
     
     /**
-     * Returns currently displayed area
-     * @return 
+     * Paints horizontal and vertical labels if corespondent flags are set 
+     * to <b>true</b>.
+     * @param g 
+     */
+    private void paintLabels(Graphics g){
+        if(verticalLabelsFlag)
+            paintVerticalLabels(g);
+        
+        if(horizontalLabelsFlag)
+            paintHorizontalLabels(g);
+    }
+   
+    private void paintHorizontalLabels(Graphics g){
+        int offset = 10;
+        int lastY = borderRectangle.y + borderRectangle.height;
+        int y;
+        for(int i=0; (y = borderRectangle.y + horizontalGridOffset + i*horizontalGridGap)<lastY ;i++){   
+            if(y>borderRectangle.y && horizontalLabels != null && i < horizontalLabels.length)
+                g.drawString(horizontalLabels[i], borderRectangle.x-offset, y);
+        }
+    }
+    
+    private void paintVerticalLabels(Graphics g){
+        int offset = 15;
+        int lastX = borderRectangle.x + borderRectangle.width;
+        int x;
+        for(int i=0; (x = borderRectangle.x + verticalGridOffset + i*verticalGridGap)<lastX ;i++){
+            if(x>borderRectangle.x && verticalLabels != null && i < verticalLabels.length)
+                g.drawString(verticalLabels[i], x, borderRectangle.y+borderRectangle.height + offset);
+        }
+    }
+    
+    /**
+     * 
+     * @return area bordered area inside scrollPane
      */
     public Rectangle getBorderRectangle() {
         return borderRectangle;
@@ -203,5 +264,119 @@ public class GraphPane extends DrawArea{
     public void setSpaceWest(int spaceWest) {
         this.spaceWest = spaceWest;
     }
-  
+    /**
+     * @return number of pixels between horizontal grid lines
+     */
+    public int getHorizontalGridGap() {
+        return horizontalGridGap;
+    }
+    /**
+     * Sets number of pixels between horizontal grid lines
+     * @param horizontalGridGap 
+     */
+    public void setHorizontalGridGap(int horizontalGridGap) {
+        this.horizontalGridGap = horizontalGridGap;
+    }
+    /**
+     * 
+     * @return number of pixels between border and first horizontal grid line
+     */
+    public int getHorizontalGridOffset() {
+        return horizontalGridOffset;
+    }
+    
+    /**
+     * Sets number of pixels between border and first horizontal grid line.
+     * @param horizontalGridOffset 
+     */
+    public void setHorizontalGridOffset(int horizontalGridOffset) {
+        this.horizontalGridOffset = horizontalGridOffset;
+    }
+    /**
+     * @return number of pixels between vertical grid lines
+     */
+    public int getVerticalGridGap() {
+        return verticalGridGap;
+    }
+    /**
+     * Sets number of pixels between vertical grid lines
+     * @param verticalGridGap 
+     */
+    public void setVerticalGridGap(int verticalGridGap) {
+        this.verticalGridGap = verticalGridGap;
+    }
+    /**
+     * 
+     * @return number of pixels between border and first vertical grid line
+     */
+    public int getVerticalGridOffset() {
+        return verticalGridOffset;
+    }
+    /**
+     * Sets number of pixels between border and first vertical grid line.
+     * @param verticalGridOffset 
+     */
+    public void setVerticalGridOffset(int verticalGridOffset) {
+        this.verticalGridOffset = verticalGridOffset;
+    }
+    /**
+     * 
+     * @return array of horizontal labels 
+     */
+    public String[] getHorizontalLabels() {
+        return horizontalLabels;
+    }
+    /**
+     * Sets horizontal labels
+     * @param horizontalLabels 
+     */
+    public void setHorizontalLabels(String[] horizontalLabels) {
+        this.horizontalLabels = horizontalLabels;
+    }
+    /**
+     * 
+     * @return array of vertical labels 
+     */
+    public String[] getVerticalLabels() {
+        return verticalLabels;
+    }
+    
+    /**
+     * Sets vertical labels
+     * @param verticalLabels 
+     */
+    public void setVerticalLabels(String[] verticalLabels) {
+        this.verticalLabels = verticalLabels;
+    }
+    /**
+     * 
+     * @return vertical label's flag
+     */
+    public boolean isVerticalLabelsFlag() {
+        return verticalLabelsFlag;
+    }
+    /**
+     * Sets vertical label's flag. If <b>true</b> labels are displayed.
+     * @param verticalLabelsFlag 
+     */
+    public void setVerticalLabelsFlag(boolean verticalLabelsFlag) {
+        this.verticalLabelsFlag = verticalLabelsFlag;
+    }
+    /**
+     * 
+     * @return horizontal label's flag
+     */
+    public boolean isHorizontalLabelsFlag() {
+        return horizontalLabelsFlag;
+    }
+    /**
+     * Sets horizontal label's flag. If <b>true</b> labels are displayed.
+     * @param horizontalLabelsFlag 
+     */
+    public void setHorizontalLabelsFlag(boolean horizontalLabelsFlag) {
+        this.horizontalLabelsFlag = horizontalLabelsFlag;
+    }
+    
+    
+    
 }
