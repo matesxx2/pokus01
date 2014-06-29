84f29fc3c31230fab45558dc7afaba0c287362a7
Petr Kramar <kramar@masquerade.cz>
2014-06-29 20:39:13 +0200

    Added settings of horizontal grid lines

--- src/simplegraph/base/GraphSettings.java
+++ src/simplegraph/base/GraphSettings.java
@@ -9,36 +9,87 @@ package simplegraph.base;
 /**
  * 
  * 
- * @author petr
+ * @author Martin Kramar
  */
 public class GraphSettings {
-    
-    
-    
+     
     private static final int NUM_DISP_TIMES_DEFAULT = 10;
+    private static final int OFFSET_DEFAULT = 20;
+    private static final int GAP_DEFAULT = 80;
     
     private TimeLevel timeLevel = TimeLevel.DAY;
     private int numOfDislplayedUnits = NUM_DISP_TIMES_DEFAULT;
     
+    private int horizontalGridOffset = OFFSET_DEFAULT;
+    private int horizontalGridGap = GAP_DEFAULT;
+    
     public GraphSettings(){
         
     }
-
+    /**
+     * Timelevel sets units of x-axis
+     * @return 
+     */
     public TimeLevel getTimeLevel() {
         return timeLevel;
     }
-
+    /**
+     * Timelevel sets units of x-axis 
+     * @param timeLevel 
+     */
     public void setTimeLevel(TimeLevel timeLevel) {
         this.timeLevel = timeLevel;
     }
-
+    
+    /**
+     * Returns number vertical grid lines
+     * @return 
+     */
     public int getNumOfDislplayedUnits() {
         return numOfDislplayedUnits;
     }
-
+    
+    /**
+     * Sets number of vertical grid lines
+     * @param numOfDislplayedTimes 
+     */
     public void setNumOfDislplayedUnits(int numOfDislplayedTimes) {
         this.numOfDislplayedUnits = numOfDislplayedTimes;
     }
+    /**
+     * 
+     * @return space (in pixels) between north border of graphArea and
+     * first horizontal grid line
+     */
+    public int getHorizontalGridOffset() {
+        return horizontalGridOffset;
+    }
+    /**
+     * Sets space (in pixels) between north border of graphArea and
+     * first horizontal grid line.
+     * @param horizontalGridOffset in pixels
+     */
+    public void setHorizontalGridOffset(int horizontalGridOffset) {
+        this.horizontalGridOffset = horizontalGridOffset;
+    }
+    /**
+     * 
+     * @return size (in pixels) between horizontal grid lines
+     */
+    public int getHorizontalGridGap() {
+        return horizontalGridGap;
+    }
+    
+    /**
+     * Sets size (in pixels) between horizontal grid lines
+     * @param horizontalGridGap in pixels
+     */
+    public void setHorizontalGridGap(int horizontalGridGap) {
+        this.horizontalGridGap = horizontalGridGap;
+    }
+
+   
+    
     
     
 }
